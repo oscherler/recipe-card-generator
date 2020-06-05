af0a38677237abefb2ee9175ac5c13fa95701b68
Olivier Scherler <olivier.scherler@epfl.ch>
2020-06-06 00:48:18 +0200

    Switched to side layout.

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@ DOCKER = docker run --rm --volume "$(shell pwd):/data" recipe:latest
 PANDOC = $(DOCKER) pandoc
 PRINCE = $(DOCKER) prince
 GS = $(DOCKER) gs
+PYTHON = venv/bin/python
+PIP = venv/bin/pip
 
 ASSET_DIR = assets
 RECIPE_DIR = recipes
@@ -9,11 +11,11 @@ IMAGES_DIR = images
 TEMP_DIR = tmp
 CARDS_DIR = cards
 
-CSS_NAME = recipe
+CSS_NAME = recipe-side
 SCSS = $(ASSET_DIR)/$(CSS_NAME).scss
 CSS = $(ASSET_DIR)/$(CSS_NAME).css
 FILTER = $(ASSET_DIR)/recipe.php
-CARD_TEMPLATE = $(ASSET_DIR)/recipe.card.html
+INT_TEMPLATE = $(ASSET_DIR)/recipe.int.html
 IMAGE_PATH = ../$(IMAGES_DIR)
 
 RECIPES = $(wildcard $(RECIPE_DIR)/*.md)
@@ -27,8 +29,11 @@ cards: $(PDFS)
 $(CSS): $(SCSS)
 	scss $< $@
 
-$(TEMP_DIR)/%.html: $(RECIPE_DIR)/%.md $(FILTER) $(CARD_TEMPLATE)
-	$(PANDOC) --standalone --section-divs --filter $(FILTER) --template $(CARD_TEMPLATE) --variable image_path:$(IMAGE_PATH) --to html5 --output $@ $<
+$(TEMP_DIR)/%.int.html: $(RECIPE_DIR)/%.md $(INT_TEMPLATE)
+	$(PANDOC) --standalone --section-divs --template $(INT_TEMPLATE) --variable image_path:$(IMAGE_PATH) --to html5 --output $@ $<
+
+$(TEMP_DIR)/%.html: $(TEMP_DIR)/%.int.html $(ASSET_DIR)/intermediate.py
+	$(PYTHON) $(ASSET_DIR)/intermediate.py $< $@
 
 $(CARDS_DIR)/%.pdf: $(TEMP_DIR)/%.html $(CSS)
 	$(PRINCE) --style $(CSS) --output $@ $<
@@ -37,10 +42,14 @@ $(CARDS_DIR)/%.pdf: $(TEMP_DIR)/%.html $(CSS)
 clean:
 	rm -f $(HTMLS) $(PDFS)
 
-.PRECIOUS: $(TEMP_DIR)/%.html
+.PRECIOUS: $(TEMP_DIR)/%.int.html $(TEMP_DIR)/%.html
 
 debug:
 	$(info $(HTMLS_FOR_INDEX))
 
 docker:
 	docker image build --tag recipe .
+
+venv:
+	python3 -mvenv venv
+	$(PIP) install -r requirements.txt
---
+++ assets/intermediate.py
@@ -0,0 +1,103 @@
+# -*- coding: utf8 -*-
+
+import sys
+import os
+from lxml import etree as ET
+from copy import deepcopy
+
+XHTML = 'http://www.w3.org/1999/xhtml'
+
+XHTML_BODY = '{' + XHTML + '}body'
+XHTML_SECTION = '{' + XHTML + '}section'
+XHTML_UL = '{' + XHTML + '}ul'
+XHTML_HR = '{' + XHTML + '}hr'
+XHTML_EM = '{' + XHTML + '}em'
+
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+
+tree = ET.parse( input_path )
+root = tree.getroot()
+steps = root.find( XHTML_BODY + '/' + XHTML_SECTION + '[@class="steps"]' )
+
+out_steps = ET.Element('section')
+out_steps.attrib['id'] = 'ingredients'
+out_steps.attrib['class'] = 'level1 ingredients'
+
+table = ET.SubElement( out_steps, 'table' )
+tbody = ET.SubElement( table, 'tbody' )
+
+new_step = True
+beginning = True
+
+for el in steps:
+    # <hr> is the step separator
+    # it’s optional, <ul> also starts a new step
+    if el.tag == XHTML_HR:
+        new_step = True
+        continue
+
+    if new_step and not beginning:
+        row = ET.SubElement( tbody, 'tr', Class='spacer' )
+        ET.SubElement( row, 'td', colspan='3' )
+
+    beginning = False
+
+    # if a step doesn’t start with a <ul>, it’s a step without ingredients
+    if new_step and el.tag != XHTML_UL:
+        row = ET.SubElement( tbody, 'tr' )
+        step_cell = ET.SubElement( row, 'td', colspan='3', Class='step' )
+        step_cell.append( deepcopy( el ) )
+        new_step = False
+        continue
+
+    # <ul>: ingredients
+    if el.tag == XHTML_UL:
+        # span the text <td> on all the ingredient rows
+        step_cell = ET.Element( 'td', rowspan=str( len( el ) ), Class='steps' )
+
+        first = True
+        for ingredient in el:
+            row = ET.SubElement( tbody, 'tr' )
+            quantity_cell = ET.Element( 'td', Class='quantity' )
+            ingredient_cell = ET.SubElement( row, 'td', Class='ingredient' )
+            has_quantity = False
+
+            if len( ingredient ) == 0:
+                # no sub elements, just text, so no quantity
+                ingredient_cell.text = ingredient.text
+            else:
+                # sub elements, check for <em> for quantity
+                for ingredient_el in ingredient:
+                    if ingredient_el.tag == XHTML_EM:
+                        has_quantity = True
+
+                        # text after last element is in last element tail, move it so ingredient text
+                        ingredient_cell.text = ingredient_el.tail
+                        ingredient_el.tail = None
+
+                        # insert quantity cell before ingredient cell
+                        quantity_cell.append( deepcopy( ingredient_el ) )
+                        row.insert( 0, quantity_cell )
+                    else:
+                        ingredient_cell.append( deepcopy( ingredient_el ) )
+                    
+            if not has_quantity:
+                ingredient_cell.attrib['colspan'] = '2'
+            
+            if first:
+                row.append( step_cell )
+
+            first = False
+
+    else:
+        step_cell.append( deepcopy( el ) )
+        pass
+
+    new_step = False
+
+parent = steps.getparent()
+parent.insert( parent.index( steps ), out_steps )
+steps.getparent().remove( steps )
+
+tree.write( output_path )
